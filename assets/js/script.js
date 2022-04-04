547a6d7a057dcb5aad5029aec30cf580030bd90c
VSartz <elsartz@gmail.com>
2022-04-03 21:01:13 -0400

    defragmentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,7 +93,34 @@ console.log(conditions);
 
 }
 
-
+function nextPosition(lat, lon) {
+  console.log(lat, lon);
+    // var latitude = position.coords.latitude;
+    // var longitude = position.coords.longitude;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=metric&appid=5d3247362c5bea55d3c0e663cb0344b0";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+          response.json().then(function(data) {
+            console.log(data);
+            var localArea = data.timezone;
+            var location = localArea.split("/")[1];
+              console.log(location);
+  
+            displayLocalWeather(data, location);
+            display5Days(data);
+          });
+        } else {
+          // unrecognizable city name
+          alert("Error: City not found");
+        }
+      })
+      .catch(function(error) {
+        // If no response then report network error
+        alert("Unable to connect to OpenWeather API");
+      });
+  
+  }
 
 
 
@@ -107,7 +134,11 @@ var getUserSearch = function(location) {
       if (response.ok) {
         response.json().then(function(dataU) {
           console.log(dataU);
-          displayWeather(dataU, location);
+          var lat = dataU.coord.lat;
+          var lon = dataU.coord.lon;
+          nextPosition(lat, lon);
+          console.log(lat,lon);
+          // displayWeather(dataU, location);
         });
       } else {
         // unrecognizable city name
@@ -150,40 +181,6 @@ var resetCard = function() {
 }
 
 
-var displayWeather = function(conditions, searchTerm) {
-
-  var currentDate = moment().format('dddd, MMM Do YYYY');       
-  console.log(searchTerm);    console.log(currentDate);
-
-  resetCard();
-
-  
-  // var cityEl = document.createElement("h3");
-      cityEl.textContent = searchTerm + " " + currentDate;
-      locationEl.appendChild(cityEl);
-
-  // var tempEl = document.createElement("p");
-      tempEl.textContent = "Temperature :  " + Math.floor(conditions.main.temp) + " C";
-      locationEl.appendChild(tempEl);
-
-  // var windEl = document.createElement("p");
-      windEl.textContent = "Wind speed :  " + Math.floor(conditions.wind.speed) + " km/h";
-      locationEl.appendChild(windEl);
-
-  // var humidEl = document.createElement("p");
-      humidEl.textContent = "Humidity :  " + conditions.main.humidity + " %";
-      locationEl.appendChild(humidEl);
-
-
-  //  console.log(conditions);
-   
-   console.log(conditions.main.temp);
-   console.log(conditions.wind.speed);
-   console.log(conditions.main.humidity);
-   
-  
-}
-
 var formSubmitHandler = function(event) {
 
   event.preventDefault();         // prevent to send data to a url
@@ -216,10 +213,10 @@ console.log(formEl0);
 
 
 var display5Days = function(conditions) {
-
+    
   for (var i=0; i < 5; i++) {
     var dailyDivEl = document.createElement("div");
-
+        
     var dailyDate = moment().add(1+i, "days").format('dddd, MMM Do YYYY');
     var humidity = conditions.daily[i].humidity;
     var temp = Math.round(conditions.daily[i].temp.day);
@@ -248,6 +245,7 @@ var display5Days = function(conditions) {
 var historySubmitHandler = function(event) {
   event.preventDefault();
   console.log("event");
+
 } 
 
 // get current location
@@ -256,5 +254,5 @@ getLocation();
 // get users city search
 userFormEl.addEventListener("submit", formSubmitHandler);
 console.log(userFormEl);
-historyEl.addEventListener("submit", historySubmitHandler);
+historyEl.addEventListener("click", historySubmitHandler);
 console.log(historyEl);
